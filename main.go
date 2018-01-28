7e58663539b8ed30e4ba7db6d9b3d3051517b135
Renzo Crisostomo <ruenzuo@gmail.com>
2018-01-28 18:07:33 +0100

    Add calc command and username flag

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
-  "os"
-  "gopkg.in/urfave/cli.v1"
+	"fmt"
+	"os"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-  cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Commands = []cli.Command{
+		{
+			Name:        "calc",
+			Usage:       "nisekoi calc [<github-org> | <github-org/repo>]",
+			Description: "Calculate average landing PR times",
+			Action: func(c *cli.Context) error {
+				username := c.String("username")
+				fmt.Println(fmt.Sprintf("%s", username))
+				return nil
+			},
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "username, u",
+					Usage:  "If set, average times for `USERNAME` will be displayed",
+					EnvVar: "NISEKOI_USERNAME",
+				},
+			},
+		},
+	}
+	app.Run(os.Args)
 }
